213e56ef2d98922c33f2353201db6a6b4b92c4f8
davidboybob <davidboy7780@gmail.com>
2019-11-21 11:44:36 +0900

    dummy, forms, like done

--- project/movies/views.py
+++ project/movies/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
-from .models import Movie
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+from .models import Movie, Review
 from .forms import ReviewForm
 
 # Create your views here.
@@ -17,3 +20,35 @@ def detail(request, movie_pk):
     
     context = {'movie': movie, 'reviews': reviews, 'review_form': review_form}
     return render(request, 'movies/detail.html', context)
+
+@require_POST
+def review_create(request, movie_pk):
+    if request.user.is_authenticated:
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.movie_id = movie_pk
+            review.user = request.user
+            review.save()
+
+    return redirect('movies:detail', movie_pk)
+
+@require_POST
+def review_delete(request, movie_pk, review_pk):
+    if request.user.is_authenticated:
+        review = get_object_or_404(Review, pk=review_pk)
+        if request.user == review.user:
+            review.delete()
+        return redirect('movies:detail', movie_pk)
+    return HttpResponse('작성한 댓글이 아닙니다.', status=401)
+
+@login_required
+def like(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+
+    if movie.like_users.filter(pk=request.user.pk).exists():
+        movie.like_users.remove(request.user)
+    else:
+        movie.like_users.add(request.user)
+    return redirect('movies:index')
+
